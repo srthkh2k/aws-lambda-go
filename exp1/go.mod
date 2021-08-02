0a94924f3b08884feb0c8369e94d1fd6301f1c46
Ubuntu <ubuntu@ip-172-31-37-91.ap-south-1.compute.internal>
2021-08-03 01:29:29 +0530

    First test example to get familiar with gui

---
+++ exp1/go.mod
@@ -0,0 +1,5 @@
+module github.com/srthkh2k/aws-lambda-go
+
+go 1.16
+
+require github.com/aws/aws-lambda-go v1.26.0
---
+++ exp1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(Handler)
+}
+
+func Handler() {
+	fmt.Println("Function invoked!")
+}
